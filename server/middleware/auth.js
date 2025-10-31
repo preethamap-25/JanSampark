18a13a4aee1890a768a85ff265ac58adc36fed76
preethamap-25 <preetham.aap@gmail.com>
2025-11-01 03:38:38 +0530

    minor changes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-  const token = req.header('Authorization')?.split(' ')[1]; // Expecting "Bearer <token>"
+  const token = req.header('Authorization')?.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ error: 'No token, authorization denied' });
